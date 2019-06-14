57762f164a2ae828cc9bb25581ecda9216f02a48
Maasa Kono <konomaasa@gmail.com>
2019-06-14 21:57:28 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,12 @@
+#this method squares each element in an array of numbers
+#returns a new array of the squared numbers
+
 def square_array(array)
   # your code here
+  squared_array = []
+  array.each do |num|
+    squared_num = num**2
+    squared_array << squared_num
+  end
+  squared_array
 end
